36fa2121f02a8e995fbf9f093e9344a7bff84dcc
edwinarredondo <edwin.arredondo@udea.edu.co>
2020-05-08 10:52:34 -0500

    step3_ok

--- app/src/main/java/com/example/projectV3S/NewReservaStep2.kt
+++ app/src/main/java/com/example/projectV3S/NewReservaStep2.kt
@@ -1,21 +1,30 @@
 package com.example.projectV3S
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.Toast
+import com.example.projectV3S.Room.NewResRoom
 import com.example.projectV3S.UTILS.Constantes
+import com.example.projectV3S.model.NuevaReservRoom
+import com.example.projectV3S.model.Usuario
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_new_reserva_step2.*
 
 
 
 class NewReservaStep2 : AppCompatActivity() {
 
-    var num_particif1: String = Constantes.EMPTY
-
+    var num_particif1 : String = Constantes.EMPTY
+    var numpartici = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_reserva_step2)
@@ -23,13 +32,24 @@ class NewReservaStep2 : AppCompatActivity() {
         var dataf1 = intent?.extras
         num_particif1 = dataf1!!.getString("Numpartici").toString()
 
-        tv_partici_step2.text = "El numero de Praticipantes es " + num_particif1
+        tv_partici_step2.text = "El numero de Participantes es " + num_particif1
 
         SetearSpinnersyeditt()
 
         bt_conti_to_step3.setOnClickListener {
 
+            Comprobacionlayers()
+            if (numpartici == num_particif1.toInt()){
+                Guardar_partici_ROOM()
+                Toast.makeText(this, "BIIIIENNN", Toast.LENGTH_SHORT).show()
+                numpartici = 0
+                intent = Intent(this, NewReservaStep3::class.java)
+                intent.putExtra("Numparticif1", num_particif1)
+                startActivity(intent)
 
+
+            }
+            if (numpartici != num_particif1.toInt()) numpartici = 0
         }
 
 
@@ -53,12 +73,11 @@ class NewReservaStep2 : AppCompatActivity() {
         sp_cedula_step2_14.id = 14
         sp_cedula_step2_15.id = 15
         sp_cedula_step2_16.id = 16
-        for (i in 1..num_particif1.toInt()) {
+        for (i in 1..num_particif1.toInt()){
             //var keystring1 = "sp_cedula_step2_" + i.toString()
             //sp_cedula_step2_${i}.visibility = View.VISIBLE
             //keystring1.visibility = View.VISIBLE
             findViewById<Spinner>(i).visibility = View.VISIBLE
-
         }
 
         et_user_num_cedu_step2_1.id = 21
@@ -77,40 +96,119 @@ class NewReservaStep2 : AppCompatActivity() {
         et_user_num_cedu_step2_14.id = 34
         et_user_num_cedu_step2_15.id = 35
         et_user_num_cedu_step2_16.id = 36
-        for (j in 21..(20 + num_particif1.toInt())) {
-            //var keystring1 = "sp_cedula_step2_" + i.toString()
-            //sp_cedula_step2_${i}.visibility = View.VISIBLE
-            //keystring1.visibility = View.VISIBLE
+        for (j in 21..(20 + num_particif1.toInt())){
             findViewById<EditText>(j).visibility = View.VISIBLE
         }
-        Comprobacionlayers()
     }
 
+    @SuppressLint("ResourceType")
     private fun Comprobacionlayers() {
 
-        for (i in 21..(20 + num_particif1.toInt())) {
+        et_user_num_cedu_step2_1.id = 21
+        et_user_num_cedu_step2_2.id = 22
+        et_user_num_cedu_step2_3.id = 23
+        et_user_num_cedu_step2_4.id = 24
+        et_user_num_cedu_step2_5.id = 25
+        et_user_num_cedu_step2_6.id = 26
+        et_user_num_cedu_step2_7.id = 27
+        et_user_num_cedu_step2_8.id = 28
+        et_user_num_cedu_step2_9.id = 29
+        et_user_num_cedu_step2_10.id = 30
+        et_user_num_cedu_step2_11.id = 31
+        et_user_num_cedu_step2_12.id = 32
+        et_user_num_cedu_step2_13.id = 33
+        et_user_num_cedu_step2_14.id = 34
+        et_user_num_cedu_step2_15.id = 35
+        et_user_num_cedu_step2_16.id = 36
 
-            findViewById<EditText>(i).setOnClickListener {
 
-                val num_docu = findViewById<EditText>(i).text.toString()
-                if (num_docu.isNotEmpty()) {
-                    for (k in 21..(i-1) step 1) {
-                        val new_num = findViewById<EditText>(k).text.toString()
-                        if (new_num.isNotEmpty() && num_docu == new_num) {
-                            Toast.makeText(this, "Documento ya ingresado", Toast.LENGTH_SHORT).show()
-                            findViewById<EditText>(i).setText("")
+        for (j in 21..(20 + num_particif1.toInt())){
+
+            var vari1 : String= findViewById<EditText>(j).text.toString()
+            var vari2 : String
+            var flag = 0
+            if (vari1.isNotEmpty()){
+                for (k in (j + 1)..(20 + num_particif1.toInt())){
+                    vari2 = findViewById<EditText>(k).text.toString()
+                    if(vari2.isNotEmpty()){
+                        if (vari1 == vari2){
+                            Toast.makeText(this, "Usuario no registrado", Toast.LENGTH_SHORT).show()
+                            findViewById<EditText>(k).setText(Constantes.EMPTY)
+                            flag = 1
                         }
 
-
                     }
-
-
+                }
+                if(flag!= 1){
+                    verificacionfireb(vari1, j)
                 }
             }
-
-
         }
 
+    }
+
+    private fun verificacionfireb(searchText: String?, j: Int) {
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("usuarios")
+        var existedocu = false
+
+        myRef.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                for (snapshot in dataSnapshot.children){
+                    val usertodocu = snapshot.getValue(Usuario::class.java)
+                    if (usertodocu!!.num_documento.equals(searchText)){
+                        numpartici ++
+                        existedocu = true
+                    }
+                }
+                if (!existedocu){
+                    Toast.makeText(this@NewReservaStep2, "NO existe Usuario", Toast.LENGTH_SHORT).show()
+                    findViewById<EditText>(j).setText(Constantes.EMPTY)
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("quepasomal", "Failed to read value", error.toException())
+            }
+        })
 
     }
+
+    private fun Guardar_partici_ROOM() {
+
+        val vari1 : String= et_user_num_cedu_step2_1.text.toString()
+        val vari2 : String= et_user_num_cedu_step2_2.text.toString()
+        val vari3 : String= et_user_num_cedu_step2_3.text.toString()
+        val vari4 : String= et_user_num_cedu_step2_4.text.toString()
+        val vari5 : String= et_user_num_cedu_step2_5.text.toString()
+        val vari6 : String= et_user_num_cedu_step2_6.text.toString()
+        val vari7 : String= et_user_num_cedu_step2_7.text.toString()
+        val vari8 : String= et_user_num_cedu_step2_8.text.toString()
+        val vari9 : String= et_user_num_cedu_step2_9.text.toString()
+        val vari10 : String= et_user_num_cedu_step2_10.text.toString()
+        val vari11 : String= et_user_num_cedu_step2_11.text.toString()
+        val vari12 : String= et_user_num_cedu_step2_12.text.toString()
+        val vari13 : String= et_user_num_cedu_step2_13.text.toString()
+        val vari14 : String= et_user_num_cedu_step2_14.text.toString()
+        val vari15 : String= et_user_num_cedu_step2_15.text.toString()
+        val vari16 : String= et_user_num_cedu_step2_16.text.toString()
+
+        val newresDAO = NewResRoom.database1.NewresDAO()
+        val nuevaReservRoom1 = newresDAO.searchEscenari()
+
+        val canchaselec = nuevaReservRoom1.escen
+        val fechaselec = nuevaReservRoom1.fecha
+        val horaselec = nuevaReservRoom1.hora
+        val nuevareservroom2 = NuevaReservRoom(1,canchaselec , fechaselec, horaselec, participante1 = vari1, participante2 = vari2,
+            participante3 = vari3, participante4 = vari4, participante5 = vari5, participante6 = vari6,
+            participante7 = vari7, participante8 = vari8, participante9 = vari9, participante10 = vari10,
+            participante11 = vari11, participante12 = vari12, participante13 = vari13, participante14 = vari14,
+            participante15 = vari15, participante16 = vari16)
+        newresDAO.deleteReserva(nuevaReservRoom1)
+        newresDAO.insertNuevares(nuevareservroom2)
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/projectV3S/NewReservaStep3.kt
@@ -0,0 +1,174 @@
+package com.example.projectV3S
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.example.projectV3S.Room.NewResRoom
+import com.example.projectV3S.Room.NewresDAO
+import com.example.projectV3S.UTILS.Constantes
+import com.example.projectV3S.model.Reservas
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_new_reserva_step3.*
+
+class NewReservaStep3 : AppCompatActivity() {
+    var num_particif2 : String = Constantes.EMPTY
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_reserva_step3)
+
+        var dataf2 = intent?.extras
+        num_particif2 = dataf2!!.getString("Numparticif1").toString()
+
+        val newresDAO: NewresDAO = NewResRoom.database1.NewresDAO()
+        val nuevaReservRoom = newresDAO.searchEscenari()
+
+        tv_escen.text = nuevaReservRoom.escen
+        tv_fech.text = nuevaReservRoom.fecha
+        tv_hor.text = nuevaReservRoom.hora
+        tv_part1.text = nuevaReservRoom.participante1
+        tv_part2.text = nuevaReservRoom.participante2
+        tv_part3.text = nuevaReservRoom.participante3
+        tv_part4.text = nuevaReservRoom.participante4
+        tv_part5.text = nuevaReservRoom.participante5
+        tv_part6.text = nuevaReservRoom.participante6
+        tv_part7.text = nuevaReservRoom.participante7
+        tv_part8.text = nuevaReservRoom.participante8
+        tv_part9.text = nuevaReservRoom.participante9
+        tv_part10.text = nuevaReservRoom.participante10
+        tv_part11.text = nuevaReservRoom.participante11
+        tv_part12.text = nuevaReservRoom.participante12
+        tv_part13.text = nuevaReservRoom.participante13
+        tv_part14.text = nuevaReservRoom.participante14
+        tv_part15.text = nuevaReservRoom.participante15
+        tv_part16.text = nuevaReservRoom.participante16
+
+        SetearTextView()
+
+        bt_conti_to_main.setOnClickListener {
+
+            Guardar_Newreser_Firebase()
+        }
+
+    }
+
+    @SuppressLint("ResourceType")
+    private fun SetearTextView() {
+        tv_titul1.id = 1
+        tv_part1.id = 2
+        tv_titul2.id = 3
+        tv_part2.id = 4
+        tv_titul3.id = 5
+        tv_part3.id = 6
+        tv_titul4.id = 7
+        tv_part4.id = 8
+        tv_titul5.id = 9
+        tv_part5.id = 10
+        tv_titul6.id =11
+        tv_part6.id = 12
+        tv_titul7.id = 13
+        tv_part7.id = 14
+        tv_titul8.id = 15
+        tv_part8.id = 16
+        tv_titul9.id = 17
+        tv_part9.id = 18
+        tv_titul10.id = 19
+        tv_part10.id = 20
+        tv_titul11.id = 21
+        tv_part11.id = 22
+        tv_titul12.id = 23
+        tv_part12.id = 24
+        tv_titul13.id = 25
+        tv_part13.id = 26
+        tv_titul14.id = 27
+        tv_part14.id = 28
+        tv_titul15.id = 29
+        tv_part15.id = 30
+        tv_titul16.id = 31
+        tv_part16.id = 32
+        for (i in 1..(num_particif2.toInt())*2){
+
+            findViewById<TextView>(i).visibility = View.VISIBLE
+        }
+    }
+
+    private fun Guardar_Newreser_Firebase(){
+
+        val newresDAO: NewresDAO = NewResRoom.database1.NewresDAO()
+        val nuevaReservRoom = newresDAO.searchEscenari()
+
+
+        val cancha = nuevaReservRoom.escen
+        val fech =  nuevaReservRoom.fecha
+        val hora = nuevaReservRoom.hora
+        val part1 = nuevaReservRoom.participante1
+        val part2 = nuevaReservRoom.participante2
+        val part3 = nuevaReservRoom.participante3
+        val part4 = nuevaReservRoom.participante4
+        val part5 = nuevaReservRoom.participante5
+        val part6 = nuevaReservRoom.participante6
+        val part7 = nuevaReservRoom.participante7
+        val part8 = nuevaReservRoom.participante8
+        val part9 = nuevaReservRoom.participante9
+        val part10 = nuevaReservRoom.participante10
+        val part11 = nuevaReservRoom.participante11
+        val part12= nuevaReservRoom.participante12
+        val part13= nuevaReservRoom.participante13
+        val part14 = nuevaReservRoom.participante14
+        val part15= nuevaReservRoom.participante15
+        val part16= nuevaReservRoom.participante16
+
+        writeNewReser(hora, fech, cancha, part1, part2, part3, part4, part5, part6, part7, part8, part9, part10, part11, part12,
+            part13 , part14, part15, part16)
+
+      /*  if (!part1.isNullOrEmpty() && !part2.isNullOrEmpty() && !part3.isNullOrEmpty() && !part4.isNullOrEmpty() && !part5.isNullOrEmpty()
+            && !part6.isNullOrEmpty() && !part7.isNullOrEmpty() && !part8.isNullOrEmpty() && !part9.isNullOrEmpty() && !part10.isNullOrEmpty()
+            && !part11.isNullOrEmpty()&& !part12.isNullOrEmpty()&& !part13.isNullOrEmpty()&& !part14.isNullOrEmpty()&& !part15.isNullOrEmpty()&& !part16.isNullOrEmpty())
+        {
+            writeNewReser(cancha, fech, hora, part1, part2, part3, part4, part5, part6, part7, part8, part9, part10, part11, part12,
+                part13 , part14, part15, part16)
+
+
+        }*/
+
+
+
+
+
+    }
+        private fun writeNewReser(a: String?, b:String?, c: String?, d: String?,
+                                 e: String?, f: String?, g: String?, h: String?, i: String?,j:String?,k:String?,
+                                  l:String?, m:String?, n:String?, o:String?, p:String?, q:String?, r:String?, s:String?) {
+
+            val auth: FirebaseAuth
+            auth = FirebaseAuth.getInstance()
+            val user1 = auth.currentUser
+
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference("reservas")
+
+            /*val reservas = Reservas(
+                hora = a, fecha = b, escenario = c, num_doc_part1 = d, num_doc_part2 = e, num_doc_part3 = f, num_doc_part4 = g,
+                num_doc_part5 = h, num_doc_part6 = i, num_doc_part7 = j, num_doc_part8 = k, num_doc_part9 = l, num_doc_part10 = m,
+                num_doc_part11 = n, num_doc_part12 = o, num_doc_part13 = p, num_doc_part14 = q, num_doc_part15 = r, num_doc_part16 = s
+            )*/
+            val reservas = Reservas(
+                hora = a, fecha = b, escenario = c, num_doc_part1 = d, num_doc_part2 = e, num_doc_part3 = f)
+
+            myRef.child(b!!).child(c!!).child(user1!!.uid).setValue(reservas).addOnSuccessListener {
+                var intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+
+        }
+
+}
+
+
